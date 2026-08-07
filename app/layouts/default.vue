1f47b0b23a53575ec599dbb28b67e3383350af98
Brendan Bax <brendanbax@gmail.com>
2026-08-06 20:54:34 -0500

    add default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex flex-col min-h-dvh bg-neutral-50 text-neutral-950 dark:bg-neutral-950 dark:text-white">
+        <header>
+            <NavBar />
+        </header>
+        
+        <slot />
+    
+        <footer>
+            <!-- TODO: copyright footer -->
+        </footer>
+    </div>
+</template>
