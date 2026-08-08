df8e87232533079f5069cb3a64339ddeabb2950a
Brendan Bax <brendanbax@gmail.com>
2026-08-08 18:17:38 -0500

    update navbar structure

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="flex flex-col min-h-dvh bg-neutral-50 text-neutral-950 dark:bg-neutral-950 dark:text-white">
-        <header>
-            <NavBar />
-        </header>
+        <NavBar />
         
         <slot />
     
