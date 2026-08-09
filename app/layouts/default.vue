d2e59ea1f0591f68ec82ab994ca6f0a6d630c478
Brendan Bax <brendanbax@gmail.com>
2026-08-08 23:11:31 -0500

    layout fix

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col min-h-dvh">
         <NavBar />
         
-        <div class="pt-12">
+        <div class="pt-[57px] flex flex-col grow">
             <slot />
         </div>
     
