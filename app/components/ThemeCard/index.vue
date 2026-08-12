f276d23ed086f713e8c56a98b1657ec9985ac8fc
Brendan Bax <brendanbax@gmail.com>
2026-08-12 00:06:49 -0500

    add shadows

--- app/components/ThemeCard/index.vue
+++ app/components/ThemeCard/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="rounded-lg bordered border bg-base-50 dark:bg-base-950 p-4 sm:p-6">
+    <div class="rounded-lg bordered border bg-base-50 dark:bg-base-950 shadow-xl dark:shadow-brand/10 p-4 sm:p-6">
         <slot />
     </div>
 </template>
