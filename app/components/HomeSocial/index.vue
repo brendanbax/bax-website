552f6cf1a2290292676d92138b58c2063b2b0d27
Brendan Bax <brendanbax@gmail.com>
2026-08-12 22:23:15 -0500

    update social block styles

--- app/components/HomeSocial/index.vue
+++ app/components/HomeSocial/index.vue
@@ -26,15 +26,22 @@ const socialLinks = [
             <!-- Maybe an interactive canvas game on the left + the 3 social icons on the right -->
             <!-- ... endless runner game? snake? memory match? something to do with connections? -->
             <div class="pinstripes border-r border-t border-brand-500 dark:border-brand-800" />
-            <div class="flex items-center justify-center p-16">
-                <ul class="flex flex-col space-y-8">
-                    <li v-for="link in socialLinks" :key="link.href">
-                        <a :href="link.href" target="_blank" class="flex items-center gap-6">
-                            <component :is="link.icon" class="size-8" />
-                            <span class="text-xl">{{ link.label }}</span>
-                        </a>
-                    </li>
-                </ul>
+            <div class="flex flex-col lg:flex-row mt-12 lg:mt-0 items-center">
+                <div class="font-bebas text-6xl sm:text-8xl leading-none text-brand flex flex-row lg:flex-col">
+                    <span class="block mr-8 sm:mr-10 lg:mr-0">GET</span>
+                    <span class="block">SOC</span>
+                    <span class="block">IAL</span>
+                </div>
+                <div class="flex items-center justify-center p-16 w-full">
+                    <ul class="flex flex-col space-y-8">
+                        <li v-for="link in socialLinks" :key="link.href">
+                            <a :href="link.href" target="_blank" class="flex items-center gap-6">
+                                <component :is="link.icon" class="size-8" />
+                                <span class="text-xl">{{ link.label }}</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </section>
 </template>
